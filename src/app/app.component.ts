3cbd7216475119eb18c4ba589a6a46f0889e61ec
kamleshdugade <kamleesh.dugade20@vit.edu>
2021-10-18 07:24:42 +0530

    Commit as per tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import {colors} from "@angular/cli/utilities/color";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  //styles:['h3{color: dodgerblue}']
 })
 export class AppComponent {
-  title = 'my-first-app';
+  name = 'Kamlesh';
 }
